2f30da62be8701e3b1806300a65d6952d1553ab4
mouhamed-o-cisse <65414029+mouhamed-o-cisse@users.noreply.github.com>
2020-06-23 09:13:35 +0000

    changed the server expression

--- server.js
+++ server.js
@@ -3,61 +3,61 @@ const debug = require("debug")("node-angular");
 const http = require("http");
 
 
-app.listen(process.env.PORT || 3200, function() {
-  console.log("App started !");
-})
+// app.listen(process.env.PORT || 3200, function() {
+//   console.log("App started !");
+// })
 
-// const normalizePort = val => {
-//   var port = parseInt(val, 10);
+const normalizePort = val => {
+  var port = parseInt(val, 10);
 
-//   if (isNaN(port)) {
-//     // named pipe
-//     return val;
-//   }
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-//   if (port >= 0) {
-//     // port number
-//     return port;
-//   }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-//   return false;
-// };
+  return false;
+};
 
-// function onError (error) {
-//   if (error.syscall !== "listen") {
-//     throw error;
-//   }
-//   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-//   switch (error.code) {
-//     case "EACCES":
-//       console.error(bind + " requires elevated privileges");
-//       process.exit(1);
-//       break;
-//     case "EADDRINUSE":
-//       console.error(bind + " is already in use");
-//       process.exit(1);
-//       break;
-//     default:
-//       throw error;
-//   }
-// };
+function onError (error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
 
-// function onListening () {
-//   const addr = server.address();
-//   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-//   debug("Listening on " + bind);
-// };
+function onListening () {
+  const addr = server.address();
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  debug("Listening on " + bind);
+};
 
-// const port = normalizePort(process.env.PORT || "3200", function(){
-//   console.log('App started!');
-// });
-// app.set("port", port);
+const port = normalizePort(process.env.PORT || "3200", function(){
+  console.log('App started!');
+});
+app.set("port", port);
 
 
 
-// const server = http.createServer(app);
-// server.on("error", onError);
-// server.on("listening", onListening);
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
 
-// server.listen(port);
+server.listen(port);
 
