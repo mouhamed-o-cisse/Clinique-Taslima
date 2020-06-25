1f563f25b7822ad9da27a56bc8a0e5d19770a723
mouhamed-o-cisse <65414029+mouhamed-o-cisse@users.noreply.github.com>
2020-06-25 01:14:21 +0000

    specified the way by /rdv-liste

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const compression = require('compression');
 console.log({env: process.env.CLEARDB_DATABASE_URL});
 app.use((req, res, next) => {
      
-    res.setHeader('Access-Control-Allow-Origin', 'newctfrontend.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://newctfrontend.herokuapp.com/rdv-liste');
 // newctfrontend.herokuapp.com
     res.setHeader('Access-Control-Allow-Headers', 
     'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
